a0ea29029d40cabeadf526ba137cedc5fd16dc16
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-04-01 00:36:51 +0200

    fav-icon-update

--- src/components/HeadComponent.tsx
+++ src/components/HeadComponent.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 const HeadComponent = () => {
   return (
     <Head>
-      <title>Bioprof</title>
-      <meta name="description" content="Güzelliğin farkına var" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/bioprof-logo.png" />
+      <title>Gücünü Bilimden Alır - Bioprof</title>
+      <meta name='description' content='Güzelliğin farkına var' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/fav-icon.png' />
     </Head>
   );
 };
